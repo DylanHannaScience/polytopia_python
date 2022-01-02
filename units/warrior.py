f61553a86a75cb9fb835e16262f1b57740d236a1
lgriffin <lizzgriffin@hotmail.co.uk>
2022-01-02 13:49:22 +0000

    finished movement function

--- units/warrior.py
+++ units/warrior.py
@@ -1,8 +1,9 @@
-import settings
+from settings import UNIT_TYPE_CHANNEL, UNIT_STATE_CHANNEL, UNIT_TEAM_CHANNEL,\
+ UNIT_HEALTH_CHANNEL, UNIT_CHANNELS, UNIT_TYPES, MAP_HEIGHT, MAP_WIDTH
 import numpy as np
 
 class Warrior: 
-    def __init__(current_position: tuple[int, int]):
+    def __init__(self, current_position: tuple[int, int], team: int):
         self.health = 10
         self.unit_type = "warrior"
         self.max_movement_distance = 1
@@ -10,9 +11,10 @@ class Warrior:
         self.defence = 1
         self.attack_range = 1
         self.current_position = current_position
+        self.team = team
 
-    def move_char(movement_x: int, movement_y: int, map_state_array: np.ndarray):
-        map_x, map_y = settings.MAP_WIDTH, settings.MAP_HEIGHT
+    def move_char(self, movement_x: int, movement_y: int, map_state_array: np.ndarray):
+        map_x, map_y = MAP_WIDTH, MAP_HEIGHT
         current_x, current_y = self.current_position
         new_position_x = current_x + movement_x
         new_position_y = current_y + movement_y
@@ -23,9 +25,18 @@ class Warrior:
         if (new_position_x > map_x or new_position_x < 0) or (new_position_x > map_y or new_position_y < 0):
             return 
         # make sure no other unit in the way
-        if map_state_array[new_position_y, new_position_x, settings.UNIT_TYPE_CHANNEL]:
+        if map_state_array[new_position_y, new_position_x, UNIT_TYPE_CHANNEL]:
             return
 
         # do the move
-        # update
+        map_state_array[new_position_y, new_position_x, UNIT_TYPE_CHANNEL] = UNIT_TYPES[self.unit_type]
+        map_state_array[new_position_y, new_position_x, UNIT_STATE_CHANNEL] = 1
+        map_state_array[new_position_y, new_position_x, UNIT_TEAM_CHANNEL] = self.team
+        map_state_array[new_position_y, new_position_x, UNIT_HEALTH_CHANNEL] = self.health
+        # removing state from old position of unit
+        for UNIT_CHANNEL in UNIT_CHANNELS:
+            map_state_array[current_y, current_x, UNIT_CHANNEL] = 0
+        
+
+
         
